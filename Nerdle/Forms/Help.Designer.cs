59ef6a917b54ff20b1d7aa7424e30d4695bb4f12
osaama05 <oskari.makela@hotmail.com>
2022-09-06 12:46:06 +0300

    Adding the game

---
+++ Nerdle/Forms/Help.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Nerdle_Lopputyö
+{
+    partial class Help
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Help));
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.Color.DimGray;
+            this.textBox6.Location = new System.Drawing.Point(29, 365);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(50, 50);
+            this.textBox6.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.DarkOrange;
+            this.textBox3.Location = new System.Drawing.Point(29, 291);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(50, 50);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.LimeGreen;
+            this.textBox2.Location = new System.Drawing.Point(29, 215);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(50, 50);
+            this.textBox2.TabIndex = 10;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.BackColor = System.Drawing.Color.Maroon;
+            this.buttonClose.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonClose.ForeColor = System.Drawing.Color.FloralWhite;
+            this.buttonClose.Location = new System.Drawing.Point(264, 443);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(114, 33);
+            this.buttonClose.TabIndex = 8;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = false;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FloralWhite;
+            this.label1.Location = new System.Drawing.Point(94, 229);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(347, 21);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "You have the right character in the right position.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.FloralWhite;
+            this.label2.Location = new System.Drawing.Point(94, 308);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(458, 21);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "The answer contains this character but it is in a different position.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.FloralWhite;
+            this.label3.Location = new System.Drawing.Point(94, 380);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(290, 21);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "The answer doesn\'t contain this number.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.FloralWhite;
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(553, 189);
+            this.label4.TabIndex = 19;
+            this.label4.Text = resources.GetString("label4.Text");
+            // 
+            // Help
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(633, 496);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.buttonClose);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Help";
+            this.Text = "Help";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private TextBox textBox6;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private Button buttonClose;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ Nerdle/Forms/Nerdle.cs
@@ -0,0 +1,557 @@
+namespace Nerdle_Lopputyö
+{
+    public partial class Nerdle : Form
+    {
+        private TextBox lastFocused;
+        private TextBox currentlyFocused;
+        private List<TextBox> textBoxes = new();
+        private List<TextBox> currentRowsBoxes = new();
+        private List<Button> keyboard = new();
+        private string answer;
+        private string input;
+        private int currentTxtBox = 0;
+        private int currentRow = 0;
+        private int rowLength = 8;
+        private int numberOfRows = 6;
+        private Files files = new Files();
+        private Help help = null;
+        private Stats stats = null;
+
+
+        public Nerdle()
+        {
+            InitializeComponent();
+
+            StartPosition = FormStartPosition.CenterScreen;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            BackColor = Color.SlateGray;
+            MaximizeBox = false;
+
+            CreateTextBoxes();
+            CreateButtons();
+            Size = new Size(765, Convert.ToInt32(keyboard.Last().Location.Y) + 125);
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                currentRowsBoxes.Add(textBoxes[i]);
+            }
+
+            files.CreateFile(numberOfRows, "Stats.txt");
+            files.ChooseAnswer();
+            answer = files.ReturnAnswer();
+        }
+
+
+        private void CreateTextBoxes()
+        {
+            int x = 100;
+            int y = 50;
+            int row = 1;
+            int col = 1;
+
+            for (int i = 0; i < (rowLength * numberOfRows); i++)
+            {
+                if (col > rowLength)
+                {
+                    col = 1;
+                    row++;
+                    y += 55;
+                    x = 100;
+                }
+
+                TextBox txtBox = new TextBox();
+                x += 55;
+                txtBox.Location = new Point(x, y);
+                txtBox.Size = new Size(50, 50);
+                txtBox.BackColor = Color.FloralWhite;
+                txtBox.ReadOnly = true;
+                txtBox.Name = "txtBox" + row + "_" + col;
+                txtBox.TabIndex = i;
+                txtBox.Multiline = true;
+                txtBox.TextAlign = HorizontalAlignment.Center;
+                txtBox.AutoSize = true;
+                txtBox.Font = new Font("Arial", 16);
+                txtBox.Enter += new EventHandler(textBoxFocusEnter);
+                txtBox.Leave += new EventHandler(textBoxFocusLost);
+
+                col++;
+                Controls.Add(txtBox);
+                textBoxes.Add(txtBox);
+            }
+        }
+
+        private void CreateButtons()
+        {
+            int x = 100;
+            int y = Convert.ToInt32(textBoxes.Last().Location.Y) + 100;
+
+            List<string> texts = new List<string>() { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "Plus", "Minus", "Times", "Divide", "Equals", "Enter", "Delete" };
+            for (int i = 0; i < texts.Count(); i++)
+            {
+                Button btn = new Button();
+                btn.Name = "btn" + texts[i];
+                btn.Click += new EventHandler(ButtonClick);
+
+                if (texts[i] == "Enter")
+                {
+                    btn.Size = new Size(77, 50);
+                    btn.Font = new Font("Arial", 11);
+                    btn.Text = texts[i];
+                }
+
+                else if (texts[i] == "Delete")
+                {
+                    btn.Size = new Size(77, 50);
+                    btn.Font = new Font("Arial", 11);
+                    btn.Text = texts[i];
+                    x += 28;
+                }
+
+                else if (texts[i] == "Equals")
+                {
+                    btn.Size = new Size(50, 50);
+                    btn.Font = new Font("Arial", 20);
+                    btn.Text = "=";
+                    keyboard.Add(btn);
+                }
+
+                else if (texts[i] == "Times")
+                {
+                    btn.Size = new Size(50, 50);
+                    btn.Font = new Font("Arial", 20);
+                    btn.Text = "*";
+                    keyboard.Add(btn);
+                }
+
+                else if (texts[i] == "Divide")
+                {
+                    btn.Size = new Size(50, 50);
+                    btn.Font = new Font("Arial", 20);
+                    btn.Text = "/";
+                    keyboard.Add(btn);
+                }
+
+                else if (texts[i] == "Minus")
+                {
+                    btn.Size = new Size(50, 50);
+                    btn.Font = new Font("Arial", 20);
+                    btn.Text = "-";
+                    keyboard.Add(btn);
+                }
+
+                else if (texts[i] == "Plus")
+                {
+                    btn.Size = new Size(50, 50);
+                    btn.Font = new Font("Arial", 20);
+                    x = 155;
+                    y += 55;
+                    btn.Text = "+";
+                    keyboard.Add(btn);
+                }
+
+                else
+                {
+                    btn.Size = new Size(50, 50);
+                    btn.Font = new Font("Arial", 15);
+                    btn.Text = texts[i];
+                    keyboard.Add(btn);
+                }
+
+                btn.Location = new Point(x, y);
+                x += 55;
+                btn.BackColor = Color.FloralWhite;
+                Controls.Add(btn);
+            }
+        }
+
+
+        //Source: https://stackoverflow.com/questions/12005082/textbox-focus-check
+        private void load(object sender, EventArgs e)
+        {
+            foreach (TextBox box in textBoxes)
+            {
+                box.LostFocus += textBoxFocusLost;
+            }
+
+            foreach (TextBox box in textBoxes)
+            {
+                box.GotFocus += textBoxFocusEnter;
+            }
+        }
+
+        private void textBoxFocusLost(object sender, EventArgs e)
+        {
+            lastFocused = (TextBox)sender;
+        }
+
+        /// <summary>
+        /// If the textbox that got focus is on the current row, lets the textbox get the focus.
+        /// If the textbox that got focus is not on the current row, focuses back to the previous textbox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBoxFocusEnter(object sender, EventArgs e)
+        {
+            currentlyFocused = (TextBox)sender;
+
+            if (lastFocused == null)
+            {
+                textBoxes.First().Focus();
+                currentTxtBox = currentlyFocused.TabIndex;
+            }
+
+            else if (currentRowsBoxes.Contains(textBoxes[currentTxtBox]))
+            {
+                currentTxtBox = currentlyFocused.TabIndex;
+            }
+
+            else
+            {
+                lastFocused.Focus();
+                currentTxtBox = currentlyFocused.TabIndex;
+            }
+        }
+
+
+        private void ToolStripMenuItemExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ToolStripMenuItemHelp_Click(object sender, EventArgs e)
+        {
+            if (help == null || help.Text == "")
+            {
+                help = new Help();
+                help.Show();
+            }
+
+            if (CheckOpened(help.Text))
+            {
+                help.WindowState = FormWindowState.Normal;
+                help.Focus();
+            }
+
+        }
+
+        private void ToolStripMenuItemScore_Click(object sender, EventArgs e)
+        {
+            if (stats == null || stats.Text == "")
+            {
+                stats = new Stats();
+                stats.Show();
+            }
+
+            if (CheckOpened(stats.Text))
+            {
+                stats.WindowState = FormWindowState.Normal;
+                stats.Focus();
+            }
+        }
+
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            Button pressedButton = sender as Button;
+            switch (pressedButton.Name)
+            {
+                case "btn1":
+                    lastFocused.Text = "1";
+                    SwitchFocus();
+                    break;
+
+                case "btn2":
+                    lastFocused.Text = "2";
+                    SwitchFocus();
+                    break;
+
+                case "btn3":
+                    lastFocused.Text = "3";
+                    SwitchFocus();
+                    break;
+
+                case "btn4":
+                    lastFocused.Text = "4";
+                    SwitchFocus();
+                    break;
+
+                case "btn5":
+                    lastFocused.Text = "5";
+                    SwitchFocus();
+                    break;
+
+                case "btn6":
+                    lastFocused.Text = "6";
+                    SwitchFocus();
+                    break;
+
+                case "btn7":
+                    lastFocused.Text = "7";
+                    SwitchFocus();
+                    break;
+
+                case "btn8":
+                    lastFocused.Text = "8";
+                    SwitchFocus();
+                    break;
+
+                case "btn9":
+                    lastFocused.Text = "9";
+                    SwitchFocus();
+                    break;
+
+                case "btn0":
+                    lastFocused.Text = "0";
+                    SwitchFocus();
+                    break;
+
+                case "btnPlus":
+                    lastFocused.Text = "+";
+                    SwitchFocus();
+                    break;
+
+                case "btnMinus":
+                    lastFocused.Text = "-";
+                    SwitchFocus();
+                    break;
+
+                case "btnTimes":
+                    lastFocused.Text = "*";
+                    SwitchFocus();
+                    break;
+
+                case "btnDivide":
+                    lastFocused.Text = "/";
+                    SwitchFocus();
+                    break;
+
+                case "btnEquals":
+                    lastFocused.Text = "=";
+                    SwitchFocus();
+                    break;
+
+                case "btnDelete":
+                    btnDelete_Click();
+                    break;
+
+                case "btnEnter":
+                    btnEnter_Click();
+                    break;
+            }
+        }
+
+
+        private void btnEnter_Click()
+        {
+            foreach (TextBox t in currentRowsBoxes)
+            {
+                input += t.Text;
+            }
+
+            if (input.Length == rowLength)
+            {
+                CheckAnswer();
+                currentRow++;
+
+                if (currentRowsBoxes[0].Name == "txtBox" + numberOfRows + "_1")
+                {
+                    if (input == answer)
+                    {
+                        WinEnding();
+                    }
+
+                    else
+                    {
+                        LoseEnding();
+                    }
+                }
+
+                else if (currentRowsBoxes[0].Name == "txtBox" + currentRow + "_1")
+                {
+                    SwitchRow(currentRow);
+
+                    SwitchFocus();
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Row must be full");
+                input = "";
+            }
+
+        }
+
+        private void btnDelete_Click()
+        {
+            if (currentlyFocused == currentRowsBoxes.First())
+            {
+                currentlyFocused.Text = "";
+                textBoxes[currentTxtBox].Focus();
+            }
+
+            else if (textBoxes[currentTxtBox].Text.Length == 0)
+            {
+                currentTxtBox--;
+                textBoxes[currentTxtBox].Focus();
+                currentlyFocused.Text = "";
+            }
+
+            else
+            {
+                currentlyFocused.Text = "";
+                currentlyFocused.Focus();
+            }
+        }
+
+
+        /// <summary>
+        /// Switches focus to the next textbox on the current row
+        /// </summary>
+        private void SwitchFocus()
+        {
+            if (currentlyFocused == currentRowsBoxes.Last())
+            {
+                textBoxes[currentTxtBox].Focus();
+            }
+
+            else
+            {
+                currentTxtBox++;
+                textBoxes[currentTxtBox].Focus();
+            }
+        }
+
+
+        /// <summary>
+        /// Checks if the users input was correct and changes the textboxes and buttons colors according to that
+        /// </summary>
+        private void CheckAnswer()
+        {
+            for (int i = 0; i < rowLength; i++)
+            {
+                if (input == answer)
+                {
+                    foreach (TextBox t in currentRowsBoxes)
+                    {
+                        t.BackColor = Color.LimeGreen;
+                    }
+
+                    WinEnding();
+                }
+
+                else if (answer.Contains(input[i]))
+                {
+                    if (input[i] == answer[i])
+                    {
+                        currentRowsBoxes[i].BackColor = Color.LimeGreen;
+                        currentRowsBoxes[i].ForeColor = Color.WhiteSmoke;
+
+                        foreach (Button b in keyboard)
+                        {
+                            if (Convert.ToChar(b.Text) == input[i])
+                            {
+                                b.BackColor = Color.LimeGreen;
+                                b.ForeColor = Color.WhiteSmoke;
+                                break;
+                            }
+                        }
+
+                    }
+
+                    else
+                    {
+                        currentRowsBoxes[i].BackColor = Color.DarkOrange;
+                        currentRowsBoxes[i].ForeColor = Color.WhiteSmoke;
+
+                        foreach (Button b in keyboard)
+                        {
+                            if (Convert.ToChar(b.Text) == input[i])
+                            {
+                                if (b.BackColor != Color.LimeGreen)
+                                {
+                                    b.BackColor = Color.DarkOrange;
+                                    b.ForeColor = Color.WhiteSmoke;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                else
+                {
+                    currentRowsBoxes[i].BackColor = Color.DimGray;
+                    currentRowsBoxes[i].ForeColor = Color.WhiteSmoke;
+
+                    foreach (Button b in keyboard)
+                    {
+                        if (Convert.ToChar(b.Text) == input[i])
+                        {
+                            b.BackColor = Color.DimGray;
+                            b.ForeColor = Color.WhiteSmoke;
+                            break;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        //https://stackoverflow.com/questions/3861602/how-to-check-if-a-windows-form-is-already-open-and-close-it-if-it-is
+        private bool CheckOpened(string name)
+        {
+            FormCollection fc = Application.OpenForms;
+
+            foreach (Form frm in fc)
+            {
+                if (frm.Text == name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void SwitchRow(int curRow)
+        {
+            currentRowsBoxes.Clear();
+
+            for (int i = rowLength * curRow; i < rowLength * (curRow + 1); i++)
+            {
+                currentRowsBoxes.Add(textBoxes[i]);
+            }
+
+            input = "";
+        }
+        private void WinEnding()
+        {
+            files.SaveAnswer(currentRow);
+            DialogResult result = MessageBox.Show("You were right!\nThe correct answer was: " + answer + "\nWould you like to try again?", "The End", MessageBoxButtons.YesNo);
+            CloseProgram(result);
+        }
+
+        private void LoseEnding()
+        {
+            files.SaveAnswer(numberOfRows);
+            DialogResult result = MessageBox.Show("You didn't guess the calculation :(\nThe correct answer was: " + answer + "\nWould you like to try again?", "The End", MessageBoxButtons.YesNo);
+            CloseProgram(result);
+        }
+
+        private void CloseProgram(DialogResult result)
+        {
+            if (result == DialogResult.Yes)
+            {
+                Application.Restart();
+                Environment.Exit(0);
+            }
+            else
+            {
+                Environment.Exit(0);
+
+            }
+        }
+
+    }
+    
+}
